8771afa0b814f7a6ad279446533a57c1e58ff000
PanagiotisChatzoglou <pegadeli12345@gmail.com>
2025-02-11 03:41:06 +0200

    Completed basic features for the dashboard page

---
+++ src/context/DarkModeContext.jsx
@@ -0,0 +1,46 @@
+import { createContext, useContext, useEffect } from "react";
+import { useLocalStorageState } from "../hooks/useLocalStorageState";
+
+const DarkModeContext = createContext();
+
+function DarkModeProvider({ children }) {
+  const userModePreference = window.matchMedia(
+    "(prefers-color-scheme: dark)"
+  ).matches;
+
+  const [isDarkMode, setIsDarkMode] = useLocalStorageState(
+    userModePreference,
+    "isDarkMode"
+  );
+
+  // Update the class on the html element to reflect the dark mode state
+  useEffect(() => {
+    if (isDarkMode) {
+      document.documentElement.classList.add("dark-mode");
+      document.documentElement.classList.remove("light-mode");
+    } else {
+      document.documentElement.classList.add("light-mode");
+      document.documentElement.classList.remove("dark-mode");
+    }
+  }, [isDarkMode]);
+
+  function toggleDarkMode() {
+    setIsDarkMode((isDark) => !isDark);
+  }
+
+  return (
+    <DarkModeContext.Provider value={{ isDarkMode, toggleDarkMode }}>
+      {children}
+    </DarkModeContext.Provider>
+  );
+}
+
+function useDarkMode() {
+  const context = useContext(DarkModeContext);
+  if (context === undefined) {
+    throw new Error("useDarkMode must be used within a DarkModeProvider");
+  }
+  return context;
+}
+
+export { DarkModeProvider, useDarkMode };
